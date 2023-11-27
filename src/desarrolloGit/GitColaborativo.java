688a30b38adc3e5ac94df723bead28f6bbdb968d
Pedro <pedroramirez_1991@hotmail.com>
2023-11-27 18:26:52 +0100

    Mejorando el codigo de Java

--- src/desarrolloGit/GitColaborativo.java
+++ src/desarrolloGit/GitColaborativo.java
@@ -4,43 +4,37 @@ import java.util.Scanner;
 public class GitColaborativo {
 
 	public static void main(String[] args) {
-		var sc = new Scanner(System.in);
-		
-		double descvip = 0.95;
-		
-		//cliente introduce los km
-		System.out.println("Introduzca el numero de km que ha conducido: ");
-		int km = sc.nextInt();
-		
-		//cliente introduce si es vip o no
-		System.out.println("Introduzca 1 si es VIP o 2 si no lo es: ");
-		int vip = sc.nextInt();
-		
-		//variables de tipo de facturacion
-		double p1 = (km - 300) * 0.30 + (200);
-		double p2 = (km - 1000) * 0.15 + (300 + 200);
-		
-		//Cálculo facturación y visualización del total
-		if((km < 300) && (vip == 1))
-		System.out.println("Total:" + 200 * descvip );
-		
-		else if((km < 300) && (vip == 2))
-		System.out.println("Total: " + 200);
-		
-		else if((km >=300) && (km <= 1000) && (vip == 2 ))
-		System.out.println("Total no vip: " + (p1 * 1.21));
-		
-		else if((km >= 300) && (km <= 1000) && (vip == 1))
-		System.out.println("Total:" + (p1 * 0.95) * 1.21 );
-		
-		else if((km > 1000) && (vip == 2))
-		System.out.println("Total no vip" + (p2 * 1.21));
-		
-		else if((km > 1000) && (vip == 1))
-		System.out.println("Total vip: " + (p2 * 0.95 ) * 1.21 );
-			
-		sc.close();
-	
-	}
+		Scanner sc = new Scanner(System.in);
+
+        // Aquí hay que declarar como una constante ya que ese valor no se modifica durante la aplicación
+        final double DESC_VIP = 0.95;
+
+        // Cliente introduce los km
+        System.out.println("Introduzca el numero de km que ha conducido: ");
+        int km = sc.nextInt();
+
+        // Cliente introduce si es vip o no
+        System.out.println("Introduzca 1 si es VIP o 2 si no lo es: ");
+        int vip = sc.nextInt();
+
+        // Cálculo facturación y visualización del total
+        double total = calcularFacturacion(km, vip, DESC_VIP);
+        System.out.println("Total: " + total);
+
+        sc.close();
+    }
+
+    // Método para calcular la facturación
+    private static double calcularFacturacion(int km, int vip, double descVip) {
+        double p1 = (km - 300) * 0.30 + 200;
+        double p2 = (km - 1000) * 0.15 + (300 + 200);
+
+        if (km < 300) {
+            return (vip == 1) ? 200 * descVip : 200;
+        } else if (km >= 300 && km <= 1000) {
+            return (vip == 1) ? (p1 * descVip) * 1.21 : p1 * 1.21;
+        } else {
+            return (vip == 1) ? (p2 * descVip) * 1.21 : p2 * 1.21;
+        }
 
 }
