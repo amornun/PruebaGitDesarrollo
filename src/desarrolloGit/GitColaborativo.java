ccb83d9fe5635eec3551c6f59e8465eab85dc31c
Carlos <craserom@gmail.com>
2023-11-27 18:05:54 +0100

    cambios realizados

--- src/desarrolloGit/GitColaborativo.java
+++ src/desarrolloGit/GitColaborativo.java
@@ -5,20 +5,23 @@ public class GitColaborativo {
 
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
-		
 		double descvip = 0.95;
-		
+		int km = 0;
+		int vip = 0;
+		double tipoFactura1 = 0;
+		double tipoFactura2 = 0;
+
 		//cliente introduce los km
 		System.out.println("Introduzca el numero de km que ha conducido: ");
-		int km = sc.nextInt();
+		km = sc.nextInt();
 		
 		//cliente introduce si es vip o no
 		System.out.println("Introduzca 1 si es VIP o 2 si no lo es: ");
-		int vip = sc.nextInt();
+		vip = sc.nextInt();
 		
 		//variables de tipo de facturacion
-		double p1 = (km - 300) * 0.30 + (200);
-		double p2 = (km - 1000) * 0.15 + (300 + 200);
+		tipoFactura1 = (km - 300) * 0.30 + (200);
+		tipoFactura2 = (km - 1000) * 0.15 + (300 + 200);
 		
 		//Cálculo facturación y visualización del total
 		if((km < 300) && (vip == 1))
@@ -36,7 +39,7 @@ public class GitColaborativo {
 		else if((km > 1000) && (vip == 2))
 		System.out.println("Total no vip" + (p2 * 1.21));
 		
-		else if((km > 1000) && (vip == 1))
+		else((km > 1000) && (vip == 1))
 		System.out.println("Total vip: " + (p2 * 0.95 ) * 1.21 );
 			
 		sc.close();
